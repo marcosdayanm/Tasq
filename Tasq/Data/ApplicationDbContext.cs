08f7e44e3a9ad920cf46228b916640ee05ffc5ad
marcosdayanm <marcosdayanmann@gmail.com>
2023-12-25 16:16:22 -0600

    Configuraciones iniciales y creado de la db

---
+++ Tasq/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore; // Se agregó esta picandole a DbContext
+using Tasq.Models;
+
+// Ésta clase es muy importante, nos va a permitir meter y sacar cosas de la Db cuando queramos
+// Debemos de añadir esta clase a la app para que la podamos usar, en el archivo Program.cs
+
+
+
+
+
+namespace Tasq.Data
+{
+	public class ApplicationDbContext
+	{
+		public ApplicationDbContext()
+		{
+		}
+	}
+}
+
---
+++ Tasq/Program.cs
@@ -0,0 +1,75 @@
+﻿
+// Acá es donde tenemos todas las opciones de la app, paquetes, todo el middleware, etc
+
+
+
+using Microsoft.AspNetCore.Authentication.Cookies; // Para la auternticación
+using Microsoft.AspNetCore.Identity; // Para poder inicializar el AddIdentity
+using Microsoft.EntityFrameworkCore; // UseSqlServer()
+using Tasq.Data; // Añadí picandole a ApplicationDbContext
+using Tasq.Interfaces;
+using Tasq.Models; // Para poder inicializar el AddIdentity
+using Tasq.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Con las interfaces y los Repos que añadamos, debemos de declararlos acá en Program.cs
+
+
+
+// Conectar db, en este caso de Sqlite3
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+// Identificación y usuarios
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>(); // Ésto es para los usuarios
+builder.Services.AddMemoryCache(); // Añadir para los usuarios
+builder.Services.AddSession(); // Cookie Authentication
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(); // Ésto nos debería de dejar ver usuarios
+
+
+
+
+var app = builder.Build();
+
+// Seed data
+if (args.Length == 1 && args[0].ToLower() == "seeddata")
+{
+    // await Seed.SeedUsersAndRolesAsync(app); // Usuarios y Roles
+    // Seed.SeedData(app); // Info a las db
+}
+
+
+
+
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
