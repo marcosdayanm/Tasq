f76b2623c172b3aac3a0a5b2e72e34102736d0e9
marcosdayanm <marcosdayanmann@gmail.com>
2023-12-28 03:30:37 -0600

    .

--- Tasq/obj/Debug/net6.0/Tasq.AssemblyInfo.cs
+++ Tasq/obj/Debug/net6.0/Tasq.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tasq")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55736d5d287a512e596d556c5b55e44346edf13f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tasq")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tasq")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
